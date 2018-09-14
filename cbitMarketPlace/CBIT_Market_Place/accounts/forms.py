d4cbe1ae109b9726d50c0a96387c4b25e9473ad3
neelkanthpoosa <35606645+neelkanthpoosa@users.noreply.github.com>
2018-09-14 19:59:40 +0530

    Add files via upload

---
+++ cbitMarketPlace/CBIT_Market_Place/accounts/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from phonenumber_field.modelfields import PhoneNumberField
+from .models import UserProfile
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username',
+                'first_name',
+                'last_name',
+                'email',
+                'password1',
+                'password2',
+                )
+
+    def save(self,commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
+
+class EditProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'password',
+            )
+
+
+class ProfileForm(forms.ModelForm):
+    description = forms.CharField(required=True)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+        'description',
+        'phone',
+        'image',
+        )           
